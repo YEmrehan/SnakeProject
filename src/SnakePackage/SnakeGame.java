000a2cad8995f20319736d0a7d60b3e608c58e71
Emrehan Yurdakul <yurdakulemrehan@gmail.com>
2025-01-31 23:27:31 +0300

    Initial commit

---
+++ src/SnakePackage/SnakeGame.java
@@ -0,0 +1,9 @@
+package SnakePackage;
+public class SnakeGame 
+{
+    //https://www.youtube.com/watch?v=bI6e6qjJ8JQ
+    public static void main(String[]args)
+    {
+        new GameFrame();
+    }
+}
